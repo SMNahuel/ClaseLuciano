277898c4ce69c2bb9978cdc1c679941227fc4d1e
Luciano Piumatti <lucianopiumatti@gmail.com>
2024-01-05 11:27:31 -0300

    empiesa el commit

---
+++ js/scrypy.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("form")
+        nextbtn= document.querySelector(".nextbtn")
+        backbtn= document.querySelector(".backbtn")
+        allInput= document.querySelectorAll(".first input");
+
+nextbtn.addEventListener("click",() =>{
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');            
+        }else{
+            form.classList.remove('secActive');
+            alert("input is empty");
+        }
+    })
+})
